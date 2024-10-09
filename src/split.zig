49d1c104b77beaf65377609d8ee5f3912af149cf
Constantin Bilz <cbilz@posteo.net>
2024-10-09 13:39:32 +0200

    Add unit tests for split.zig

--- src/split.zig
+++ src/split.zig
@@ -26,8 +26,10 @@ pub fn main() void {
     const stderr = error_retaining_writer.writer();
 
     stderr.writeAll("Reading secret from stdin...\n") catch {};
-    const secret = readSecret(allocator, std.io.getStdIn().reader()) catch
-        error_handling.stdin_failed();
+    const secret = readSecret(allocator, std.io.getStdIn().reader()) catch |err| switch (err) {
+        error.OutOfMemory => error_handling.oom(),
+        else => error_handling.stdin_failed(),
+    };
 
     if (secret.len == 0) {
         stderr.writeAll("The secret must not be empty.\n") catch {};
@@ -59,23 +61,56 @@ pub fn main() void {
 /// Only returns an error if reading from standard input failed.
 fn readSecret(allocator: std.mem.Allocator, reader: anytype) ![]const u8 {
     var secret_list = std.ArrayList(u8).init(allocator);
-    secret_list.ensureUnusedCapacity(4096) catch |err| switch (err) {
-        error.OutOfMemory => error_handling.oom(),
-    };
+    try secret_list.ensureUnusedCapacity(4096);
 
     while (true) {
         const old_len = secret_list.items.len;
         secret_list.expandToCapacity();
         const bytes_read = try reader.readAll(secret_list.items[old_len..]);
-
         if (old_len + bytes_read != secret_list.items.len) {
             return secret_list.items[0 .. old_len + bytes_read];
         }
-
-        secret_list.ensureUnusedCapacity(1) catch |err| switch (err) {
-            error.OutOfMemory => error_handling.oom(),
-        };
+        try secret_list.ensureUnusedCapacity(1);
     }
+}
+
+test "readSecret.short secret" {
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const secret = "don't tell anyone";
+    var fbs = std.io.fixedBufferStream(secret);
+
+    const res = try readSecret(allocator, fbs.reader());
+    defer allocator.free(res);
+
+    try std.testing.expectEqualStrings(secret, res);
+}
+
+test "readSecret.long secret" {
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const secret = "don't tell anyone" ** 512;
+    var fbs = std.io.fixedBufferStream(secret);
+
+    const res = try readSecret(allocator, fbs.reader());
+    defer allocator.free(res);
+
+    try std.testing.expectEqualStrings(secret, res);
+}
+
+test "readSecret.OOM handling" {
+    var buf: [4096]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&buf);
+    const allocator = fba.allocator();
+
+    const secret = "don't tell anyone" ** 512;
+    var fbs = std.io.fixedBufferStream(secret);
+
+    try std.testing.expectError(error.OutOfMemory, readSecret(allocator, fbs.reader()));
 }
 
 /// Allocates a slice of the specified length and fills it with random bytes obtained from the
@@ -121,6 +156,43 @@ fn cryptoSecureRandomnessAlloc(allocator: std.mem.Allocator, len: usize) []const
     }
 }
 
+test "cryptoSecureRandomnessAlloc.length" {
+    const allocator = std.testing.allocator;
+
+    // We choose a buffer length that requires multiple syscalls with different request sizes.
+    const len = 299;
+
+    const buf = cryptoSecureRandomnessAlloc(allocator, len);
+    defer allocator.free(buf);
+
+    try std.testing.expectEqual(len, buf.len);
+}
+
+test "cryptoSecureRandomnessAlloc.distribution" {
+    const allocator = std.testing.allocator;
+
+    // We choose a buffer length that requires multiple syscalls with different request sizes.
+    const len = 10_000;
+
+    const buf_1 = cryptoSecureRandomnessAlloc(allocator, len);
+    defer allocator.free(buf_1);
+
+    const buf_2 = cryptoSecureRandomnessAlloc(allocator, len);
+    defer allocator.free(buf_2);
+
+    var counts = [1]u32{0} ** 256;
+    for (buf_1, buf_2) |x, y| {
+        counts[x] += 1;
+        counts[x -% y] += 1; // check for linear dependence
+    }
+
+    // The probability of this loop failing is less than 10^(-12) if the entropy is good.
+    for (counts) |count| {
+        try std.testing.expect(count >= 20);
+        try std.testing.expect(count <= 157);
+    }
+}
+
 /// Silently ignores write errors. Use an error accumulating writer with this function.
 fn printCoefficientDigest(writer: anytype, coeffs: []const u8) void {
     writer.writeAll("Random coefficients are 0x") catch {};
@@ -147,6 +219,63 @@ fn printCoefficientDigest(writer: anytype, coeffs: []const u8) void {
         " with a bit average of {d}.{d:0>2}.\n",
         .{ percent / 100, percent % 100 },
     ) catch {};
+}
+
+test "printCoefficientDigest.examples" {
+    var buf: [128]u8 = undefined;
+    var fbs = std.io.fixedBufferStream(&buf);
+    var error_retaining_writer = error_handling.error_retaining_writer(fbs.writer());
+    const writer = error_retaining_writer.writer();
+
+    printCoefficientDigest(writer, &.{0});
+    try std.testing.expectEqualStrings(
+        "Random coefficients are 0x00 with a bit average of 0.00.\n",
+        buf[0..fbs.pos],
+    );
+
+    fbs.reset();
+    printCoefficientDigest(writer, &.{0x10});
+    try std.testing.expectEqualStrings(
+        "Random coefficients are 0x10 with a bit average of 0.13.\n",
+        buf[0..fbs.pos],
+    );
+
+    fbs.reset();
+    printCoefficientDigest(writer, &.{0xff});
+    try std.testing.expectEqualStrings(
+        "Random coefficients are 0xff with a bit average of 1.00.\n",
+        buf[0..fbs.pos],
+    );
+
+    fbs.reset();
+    printCoefficientDigest(writer, &.{ 0x03, 0x06, 0x0c, 0x18, 0x30, 0x60 });
+    try std.testing.expectEqualStrings(
+        "Random coefficients are 0x03060c183060 with a bit average of 0.25.\n",
+        buf[0..fbs.pos],
+    );
+
+    fbs.reset();
+    printCoefficientDigest(writer, &.{ 0x3a, 0x04, 0xa5, 0x3b, 0xa4, 0xcd, 0x15 });
+    try std.testing.expectEqualStrings(
+        "Random coefficients are 0x3a04a5..a4cd15 with a bit average of 0.45.\n",
+        buf[0..fbs.pos],
+    );
+
+    // There were no writer errors until now.
+    try std.testing.expectEqual({}, error_retaining_writer.error_union);
+
+    fbs.reset();
+    printCoefficientDigest(writer, &.{0x00});
+    printCoefficientDigest(writer, &.{0x01});
+    printCoefficientDigest(writer, &.{0x02});
+    try std.testing.expectEqualStrings(
+        \\Random coefficients are 0x00 with a bit average of 0.00.
+        \\Random coefficients are 0x01 with a bit average of 0.13.
+        \\Random coeffic
+    ,
+        buf[0..fbs.pos],
+    );
+    try std.testing.expectError(error.NoSpaceLeft, error_retaining_writer.error_union);
 }
 
 /// Only returns an error if writing to standard output failed.
@@ -177,6 +306,31 @@ fn printShares(writer: anytype, secret: []const u8, coeffs: []const u8, shares: 
         }
         try writer.writeByte('\n');
     }
+}
+
+fn testCase(comptime name: [:0]const u8) !void {
+    var buf: [16384]u8 = undefined;
+    var fbs = std.io.fixedBufferStream(&buf);
+    const writer = fbs.writer();
+
+    const dir = "test_cases/" ++ name ++ "/";
+    const secret = @embedFile(dir ++ "secret");
+    const coefficients = @embedFile(dir ++ "coefficients");
+    const shares = @embedFile(dir ++ "shares");
+    const shares_count = @divExact(shares.len, 2 * secret.len + 4);
+
+    try printShares(writer, secret, coefficients, shares_count);
+    try std.testing.expectEqualStrings(shares, buf[0..fbs.pos]);
+}
+
+test "printShares.test cases with fixed coefficients" {
+    try testCase("translation_2_4");
+    try testCase("pure_quadratic_3_5");
+    try testCase("random_2_4");
+    try testCase("random_3_5");
+    try testCase("random_7_9");
+    try testCase("random_254_255");
+    try testCase("random_255_255");
 }
 
 fn printByteHex(writer: anytype, byte: u8) !void {
--- src/test.zig
+++ src/test.zig
@@ -1,3 +1,4 @@
 test {
     _ = @import("GF256Rijndael.zig");
+    _ = @import("split.zig");
 }
